af29c2cc5e2ace5f3b6ddbb0b2a231a98fbe74dc
Prince V G <princegracys@gmail.com>
2019-06-22 15:12:32 +0530

    commit for google sign in

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -18,7 +18,7 @@
         vm.gotoDetail = gotoDetail;
         vm.updateModel = updateModel;
         CoreService.addLoader();
-        if ($stateParams.venueDetails) {
+        if ($stateParams.searchKey) {
             CoreService.getSearchedEvents().then(function (res) {
                 localStorage["events"] = JSON.stringify(res.data.programme_list);
                 vm.events = res.data.programme_list;
