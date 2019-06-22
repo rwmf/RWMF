285a27bef510ffc33ebef253ce350cbaf53d567f
Prince V G <princegracys@gmail.com>
2019-06-22 07:24:40 +0530

    commit for venue

--- components/static/venueCtrl.js
+++ components/static/venueCtrl.js
@@ -1,64 +1,29 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('RWMF')
         .controller('VenueController', VenueController);
-    VenueController.$inject = ['$rootScope', '$scope', "CoreService", "$stateParams"];
+    VenueController.$inject = ['$rootScope', '$scope', "CoreService", "$stateParams", "$timeout"];
 
-    function VenueController($rootScope, $scope, CoreService, $stateParams) {
+    function VenueController($rootScope, $scope, CoreService, $stateParams, $timeout) {
         var vm = this;
         $rootScope.mainHeader = "Venue Locator";
         $rootScope.pageName = "home";
         angular.element('.sidenav-overlay').remove();
-        var cities = [{
+        var cities = [
+            {
                 "id": "1",
                 "name": "Theatre Stage",
                 "description": "Theatre Stage",
                 "location": "Rainforest World Music Festival",
-                "latitude": "1.7493987783454987",
-                "longitude": "111.31633585691452",
-                "image": ""
-            },
-            {
-                "id": "4",
-                "name": "Indigenous Stage",
-                "description": "Indigenous Stage",
-                "location": "Sarawak Cultural Village",
-                "latitude": "1.7493974378660602",
-                "longitude": "112.31673014163971",
-                "image": ""
-            },
-            {
-                "id": "5",
-                "name": "Big Tent Stage",
-                "description": "Big Tent Stage",
-                "location": "Sarawak Cultural Villag",
-                "latitude": "1.74668211939595",
-                "longitude": "113.31431078910828",
-                "image": ""
-            },
-            {
-                "id": "6",
-                "name": "Jungle Stage",
-                "description": "Jungle Stage",
-                "location": "Jungle Stage",
-                "latitude": "1.749408161701505",
-                "longitude": "114.31672477722168",
-                "image": ""
-            },
-            {
-                "id": "7",
-                "name": "Tree Stage",
-                "description": "Tree Stage",
-                "location": "Tree Stage",
-                "latitude": "1.7493974378660602",
-                "longitude": "115.31673014163971",
+                "latitude": "1.749415",
+                "longitude": "111.316273",
                 "image": ""
             }
         ];
         vm.cities = cities;
-        CoreService.setClientHeight().then(function() {
+        CoreService.setClientHeight().then(function () {
             var mapOptions;
             $rootScope.isLoading = true;
             if ($stateParams.venueDetails) {
@@ -73,27 +38,28 @@
                 $scope.infoWindow = new google.maps.InfoWindow();
                 var position = new google.maps.LatLng($stateParams.venueDetails.latitude, $stateParams.venueDetails.longitude);
                 $scope.bounds = new google.maps.LatLngBounds();
+                $scope.bounds.extend(position);
                 $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
                 createMarker($stateParams.venueDetails, $scope.infoWindow);
-                google.maps.event.addListener($scope.infoWindow, 'domready', function() {
+                google.maps.event.addListener($scope.infoWindow, 'domready', function () {
                     $('.infoWindowContent').parents(".gm-style .gm-style-iw-c").addClass("custom-marker");
                 });
-                $scope.openInfoWindow = function(e, selectedMarker) {
+                $scope.openInfoWindow = function (e, selectedMarker) {
                     e.preventDefault();
                     google.maps.event.trigger(selectedMarker, 'click');
                 }
             } else {
-                CoreService.getVenueDetails().then(function(resp) {
+                CoreService.getVenueDetails().then(function (resp) {
                     drawMap(resp.data.venues);
-                }, function(error) {
+                }, function (error) {
                     drawMap(cities);
-                }).catch(function(error) {
+                }).catch(function (error) {
                     drawMap(cities);
                 });
 
             }
         })
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             angular.element('.sidenav-overlay').remove();
         });
 
@@ -107,22 +73,21 @@
             $scope.map.setTilt(45);
             $scope.infoWindow = new google.maps.InfoWindow();
             for (i = 0; i < cities.length; i++) {
-                var position = new google.maps.LatLng(cities[i].latitude, cities[i].longitude);
-                $scope.bounds.extend(position);
                 createMarker(cities[i], $scope.infoWindow);
-                $scope.map.fitBounds($scope.bounds);
             }
-            google.maps.event.addListener($scope.infoWindow, 'domready', function() {
+
+
+            google.maps.event.addListener($scope.infoWindow, 'domready', function () {
                 $('.infoWindowContent').parents(".gm-style .gm-style-iw-c").addClass("custom-marker");
             });
-            $scope.openInfoWindow = function(e, selectedMarker) {
+            $scope.openInfoWindow = function (e, selectedMarker) {
                 e.preventDefault();
                 google.maps.event.trigger(selectedMarker, 'click');
             }
-            google.maps.event.addListenerOnce($scope.map, 'tilesloaded', function() {
+            google.maps.event.addListenerOnce($scope.map, 'tilesloaded', function () {
                 $rootScope.isLoading = false;
             });
-            google.maps.event.addDomListener(window, "resize", function() {
+            google.maps.event.addDomListener(window, "resize", function () {
                 if (document.getElementById("map")) {
                     CoreService.setClientHeight();
                     var center = $scope.map.getCenter();
@@ -133,21 +98,24 @@
         }
 
         function createMarker(info, infoWindow) {
+            var position = new google.maps.LatLng(parseInt(info.latitude), parseInt(info.longitude));
             var marker = new google.maps.Marker({
                 map: $scope.map,
-                position: new google.maps.LatLng(parseInt(info.latitude), parseInt(info.longitude)),
+                position: position,
                 title: info.location
             });
             var content = '<div class="infoWindowContent"><h4 class="info-header">' + marker.title + '</h4>' +
                 '<ul class="location">' +
                 '<li class="address mb-3"><span><b class="text-upper">' + info.description + '</b></span></li>' +
                 '</ul></div>';
-
-            google.maps.event.addListener(marker, 'click', function() {
+            $scope.bounds.extend(position);
+            $timeout(function () {
+                $scope.map.fitBounds($scope.bounds)
+            }, 500)
+            google.maps.event.addListener(marker, 'click', function () {
                 infoWindow.setContent(content);
                 infoWindow.open($scope.map, marker);
             });
-            return marker;
         }
 
     }
