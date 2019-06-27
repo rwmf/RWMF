09a3e34403b897976be68bdc7ba4a6e089823b69
Prince V G <princegracys@gmail.com>
2019-06-28 00:10:44 +0530

    commit for stable branch

--- components/static/venueCtrl.js
+++ components/static/venueCtrl.js
@@ -11,19 +11,28 @@
         $rootScope.mainHeader = "Venue Locator";
         $rootScope.pageName = "home";
         angular.element('.sidenav-overlay').remove();
-        var cities = 
-        vm.cities = cities;
+        var cities =
+            vm.cities = cities;
         vm.isNavigated = false;
         CoreService.setClientHeight().then(function () {
             vm.mapURL = $sce.trustAsResourceUrl("https://www.google.com/maps/d/embed?mid=1g4Nr2mOxgBA2jqI_FeVefdeKNncFVj8T");
             $rootScope.isLoading = true;
             if ($stateParams.venueDetails) {
-                 vm.isNavigated = true;
-                 $stateParams.venueDetails.latitude = $stateParams.venueDetails.stage_latitude;
-                 $stateParams.venueDetails.longitude = $stateParams.venueDetails.stage_longitude;
-                 $stateParams.venueDetails.location = $stateParams.venueDetails.stage_location;
-                 vm.mapURL = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyAgSkVT1uTk7XBFToLLzS50JO7UJtMujTM&q="+$stateParams.venueDetails.stage_latitude+","+$stateParams.venueDetails.stage_longitude);     
-            } 
+                vm.isNavigated = true;
+                $stateParams.venueDetails.latitude = parseFloat($stateParams.venueDetails.stage_latitude);
+                $stateParams.venueDetails.longitude = parseFloat($stateParams.venueDetails.stage_longitude);
+                $stateParams.venueDetails.location = $stateParams.venueDetails.stage_location;
+                var geocoder = new google.maps.Geocoder;
+                var latlng = { lat: $stateParams.venueDetails.latitude, lng: $stateParams.venueDetails.longitude };
+                geocoder.geocode({ 'location': latlng }, function (results, status) {
+                    if (status === 'OK' && results[0]) {
+                        vm.mapURL = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyAgSkVT1uTk7XBFToLLzS50JO7UJtMujTM&q=" + results[0].formatted_address);
+                    } else {
+                        vm.mapURL = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyAgSkVT1uTk7XBFToLLzS50JO7UJtMujTM&q=" + $stateParams.venueDetails.stage_latitude + "," + $stateParams.venueDetails.stage_longitude);
+                    }
+                });
+                
+            }
         })
         $scope.$on('$destroy', function () {
             angular.element('.sidenav-overlay').remove();
