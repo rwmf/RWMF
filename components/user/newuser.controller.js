d8c72901671acef11ba85482213c000f5b54ef1b
Abhilash SB <abhilashsb9@gmail.com>
2019-07-01 10:23:04 +0530

    Route modal hide issue fix

--- components/user/newuser.controller.js
+++ components/user/newuser.controller.js
@@ -24,6 +24,7 @@
                 $scope.openModal();
             }
             else {
+                $('#centralModalSuccess').modal('hide');
                 //$scope.closeModal();
             }
         });
