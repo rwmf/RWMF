e63dd13ea32cda1dad3b38e2da67bb0942e74d5d
Prince V G <princegracys@gmail.com>
2019-07-09 13:50:39 +0530

    commit for added meta tag

--- components/user/login.controller.js
+++ components/user/login.controller.js
@@ -82,20 +82,31 @@
         }
 
         function googleLogin() {
-            var user = {};            
-            auth2.grantOfflineAccess().then(loginCallback);
+            var user = {};
+            vm.auth2 = gapi.auth2.init({
+                client_id: '971257550676-94l84vfn2c96gq47mkqnqb8houuhd2p3.apps.googleusercontent.com',
+                scope: 'profile email'
+            });
+            var myParams = {
+                'clientid': '971257550676-94l84vfn2c96gq47mkqnqb8houuhd2p3.apps.googleusercontent.com', //You need to set client id
+                'cookiepolicy': 'single_host_origin',
+                'approvalprompt': 'force',
+                'scope': 'profile email'
+            };
+            gapi.signin2.render('myButton', myParams);
+            vm.auth2.attachClickHandler('myButton', myParams, onSignIn, onSignInFailure);
+            vm.auth2.grantOfflineAccess().then(loginCallback);
 
         }        
         
-        
+        function onSignIn(googleUser) {
+            console.log(googleUser)
+          }
+          function onSignInFailure(error) {
+            console.log(error)
+          }
         function loginCallback(test) {
             console.log(test)
         }
     }
 })();
-function onSignIn(googleUser) {
-    console.log(googleUser)
-  }
-  function onSignInFailure(error) {
-    console.log(error)
-  }
