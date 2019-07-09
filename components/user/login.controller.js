e25be82451d05e3f6108e26fef51b68ea241db8b
Prince V G <princegracys@gmail.com>
2019-07-09 11:13:26 +0530

    commit for added meta tag

--- components/user/login.controller.js
+++ components/user/login.controller.js
@@ -94,7 +94,7 @@
                 client_id: '971257550676-94l84vfn2c96gq47mkqnqb8houuhd2p3.apps.googleusercontent.com',
                 scope: 'profile email',
                 ux_mode: 'redirect',
-                redirect_uri: 'https://www.abc.io/google_login_redirect'
+                redirect_uri: 'https://rwmf.app'
             });
             console.log(vm.auth2)
             vm.auth2.signIn(myParams);
