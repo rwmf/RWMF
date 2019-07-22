fa3205d805f28f47143298650859e35cec0fabd0
Prince V G <princegracys@gmail.com>
2019-07-23 00:04:43 +0530

    commit for signup

--- components/user/login.controller.js
+++ components/user/login.controller.js
@@ -29,7 +29,7 @@
                 if (response.status == 200) {
                     fetchUserData(response.data.user_token);
                 } else {
-                    errorHandler(response);
+                    errorHandler({});
                 }
             }, function (err) {
                 errorHandler(err);
@@ -69,7 +69,7 @@
                                 }
                                 fetchUserData(response.data.user_token);
                             } else {
-                                errorHandler({});
+                                errorHandler(response);
                             }
                         }, function (err) {
                             errorHandler(err);
