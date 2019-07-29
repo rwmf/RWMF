d1dd47a219a0b096470be1a400248473103e61b0
Prince V G <princegracys@gmail.com>
2019-07-30 00:21:56 +0530

    commit for push

---
+++ push/client/main.js
@@ -0,0 +1,48 @@
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+const publicKey = 'BBX6SGuqJ-0Tdl-WuBzdf9J_oWuDtngDzfjv5JQCYExpTgO7l-WsThaUMJbsR8XrT4cOlDDTvpMXyAj_NU1EPcE';
+const privateKey = 'hXxrY4O3lrULn6L3Juv5tB0Ye8SY4jNliopX55y1Kos';
+
+const triggerPush = document.querySelector('.trigger-push');
+
+async function triggerPushNotification() {
+  if ('serviceWorker' in navigator) {
+    const register = await navigator.serviceWorker.register('/sw.js', {
+      scope: '/'
+    });
+
+    console.log('waiting for acceptance');
+    const subscription = await register.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(publicKey),
+    });
+    console.log('acceptance complete');
+
+    await fetch('/subscribe', {
+      method: 'POST',
+      body: JSON.stringify(subscription),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } else {
+    console.error('Service workers are not supported in this browser');
+  }
+}
+
+triggerPush.addEventListener('click', () => {
+  triggerPushNotification().catch(error => console.error(error));
+});
