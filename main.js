1ad08f549f89c156195de85c38264e42d554e587
Prince V G <princegracys@gmail.com>
2019-08-05 00:43:41 +0530

    push notification

--- main.js
+++ main.js
@@ -42,17 +42,13 @@ const messaging = firebase.messaging();
 messaging
     .requestPermission()
     .then(function () {
-        MsgElem.innerHTML = "Notification permission granted."
         console.log("Notification permission granted.");
-
-        // get the token in the form of promise
         return messaging.getToken()
     })
     .then(function (token) {
-        TokenElem.innerHTML = "token is : " + token
+        console.log(token);
     })
     .catch(function (err) {
-        ErrElem.innerHTML = ErrElem.innerHTML + "; " + err
         console.log("Unable to get permission to notify.", err);
     });
 
@@ -60,5 +56,4 @@ messaging.onMessage(function (payload) {
     var notificationData = JSON.parse(payload.data.notification);
     var notification = new Notification(notificationData.title, notificationData);
     console.log("Message received. ", payload);
-    NotisElem.innerHTML = NotisElem.innerHTML + JSON.stringify(payload)
 });
